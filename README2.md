ede4b103fc5603da66e436194362f32ce0e37098
Spencer_Durfey <durfey_32@yahoo.com>
2024-07-25 01:09:10 -0600

    added badge and table of contents

---
+++ README2.md
@@ -0,0 +1,42 @@
+
+# 
+
+![Static Badge](https://img.shields.io/badge/-blue) 
+ 
+
+## Description
+
+
+## Table of Contents
+
+
+
+* [License](#license)
+
+
+
+
+* [Questions?](#questions)
+
+
+
+
+
+
+
+## License
+![Static Badge](https://img.shields.io/badge/-blue) 
+
+
+
+
+
+
+
+
+
+
+## Questions?
+Github:  
+
+Email: 
--- index.js
+++ index.js
@@ -16,11 +16,6 @@ const questions = [
     },
     {
       type: 'input',
-      name: 'tableOfContents',
-      message: 'Give a list of your table of contents',
-    },
-    {
-      type: 'input',
       name: 'install',
       message: 'What are the steps required for installation?',
     },
@@ -35,14 +30,16 @@ const questions = [
       message: 'List collaborators',
     },
     {
-      type: 'input',
+      type: 'checkbox',
       name: 'license',
       message: 'What Licenses were used?',
+      choices: ['MIT', 'Apache', 'GPL', 'BSD', 'Microsoft Public License', 'CC0', 'None'],  
+      default: 'None',  
     },
     {
       type: 'input',
       name: 'badges',
-      message: 'What sort of badges were used?',
+      message: 'What is the importance of your License?',
     },
     {
       type: 'input',
@@ -59,64 +56,94 @@ const questions = [
       name: 'tests',
       message: 'Give Test Examples.',
     },
+    {
+        type: 'input',
+        name: 'Github',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
   ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const { title, description, tableOfContents, install, use, credit, license, badges, features, contribute, tests } = data;
+    const { title, description, install, use, credit, license, badges, features, contribute, tests, Github, email } = data;
     
+    // Format collaborators as a bullet list if provided
+    const collaboratorsList = credit ? credit.split(',').map(collaborator => `* ${collaborator.trim()}`).join('\n') : '';
+
     const readmeContent = `
-  # ${title}
-  
-  ## Description
-  ${description}
-  
-  ## Table of Contents
-  ${tableOfContents}
-  
-  ## Installation
-  ${install}
-  
-  ## Usage
-  ${use}
-  
-  ## Credits
-  ${credit}
-  
-  ## License
-  ${license}
-  
-  ## Badges
-  ${badges}
-  
-  ## Features
-  ${features}
-  
-  ## Contributing
-  ${contribute}
-  
-  ## Tests
-  ${tests}
-    `;
-  
+# ${title}
+
+${license ? `![Static Badge](https://img.shields.io/badge/${license}-blue) \n` : ''} 
+
+## Description
+${description}
+
+## Table of Contents
+${install ?'* [Installation](#installation)' : ''}
+${use ?'* [Usage](#usage)' : ''}
+${credit ? '* [Credits](#credits)' : ''}
+${license ? '* [License](#license)' : '' }
+${badges ? '* [Badges](#badges)' : ''}
+${features ? '* [Features](#features)' : ''}
+${contribute ? '* [Contributing](#contribute)' : ''}
+${tests ? '* [Tests](#tests)' : ''}
+* [Questions?](#questions)
+
+${install ? `
+## Installation
+${install}
+` : ''}
+
+${use ? `## Usage
+${use}
+` : ''}
+
+${credit ? `
+## Credits
+${collaboratorsList}
+` : ''}
+
+${license ? `## License
+![Static Badge](https://img.shields.io/badge/${license}-blue) \n
+${badges ? badges : ''}
+` : ''}
+
+${features ? `## Features
+${features}
+` : ''}
+
+${contribute ? `## Contributing
+${contribute}
+` : ''}
+
+${tests ? `## Tests
+${tests}
+` : ''}
+
+## Questions?
+Github: ${Github} \n
+Email: ${email}
+`;
+
     fs.writeFile(fileName, readmeContent, (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('README file generated successfully!');
-      }
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README file generated successfully!');
+        }
     });
-  }
-  
-  inquirer.prompt(questions)
+}
+// TODO: Create a function to initialize app
+function init() {  inquirer.prompt(questions)
     .then((data) => {
       const filename = `README2.md`;
       writeToFile(filename, data);
-    });
-
-
-// TODO: Create a function to initialize app
-function init() {}
+    });}
 
 // Function call to initialize app
 init();
